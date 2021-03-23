ea41d6b3e8279e1a22e324858d1204188637514c
Kserhaja <kenanserhatlic@hotmail.com>
2021-03-23 23:46:41 +0100

    parcialy

---
+++ ex04/src/index.js
@@ -0,0 +1,32 @@
+class Pets {
+    constructor(name, legs)
+        
+    
+        
+
+}
+ 
+
+    class Dog extends Pets {
+        
+    
+       
+    }
+   
+            
+    class numberOfLegs {
+
+    
+    }
+     
+    var numberOfLegs = legs
+var walk = dogSay
+
+let Dog = new Dog(name, 4);
+console.log(numberOfLegs);
+console.log(dogSay);
+
+module.exports = {
+    Pets, 
+    Dog};
+    
