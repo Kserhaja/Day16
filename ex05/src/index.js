0b5f14be449f98ecce8b111b5ca55e0868d6f0ca
Kserhaja <kenanserhatlic@hotmail.com>
2021-03-24 00:15:42 +0100

    prepravke

--- ex05/src/index.js
+++ ex05/src/index.js
@@ -1,14 +1,34 @@
-// Only change code below this line
-class Person {
-    static display() {
-        var message = "Static method is invoked from Person class.";
-        return message;
+/ Only change code below this line
+class Microsoft {
+    constructor(name) {
+      this.name = name;
     }
-     show() {
-    console.log(Person.display());
+    get name() {
+      return this._name;
     }
-    // Only change code above this line
+    set name(newName) {
+      this._name = newName;
     }
-    var message = new Person();
-    message.show();
-    module.exports = Person;
+    occupation() {
+      return this.name + " is a philanthropist!";
+    }
+  }
+  class Facebook extends Microsoft {
+    constructor(name, age) {
+      super(name);
+      this.age = age;
+    }
+    studentAge() {
+      return this.name + " is " + this.age;
+    }
+  }
+  let henry = new Microsoft("William Henry Gates III");
+  console.log(henry.occupation());
+  let elliot = new Facebook("Mark Elliot Zuckerberg", 23);
+  console.log(elliot.occupation());
+  console.log(elliot.studentAge());
+  // Only change code above this line
+  module.exports = {
+    Microsoft,
+    Facebook,
+  };
